f78dfa8808876c1e8fe9930dd0563032a693711e
teekm <tkimm@me.com>
2022-10-21 17:58:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,44 @@
+<div align="center">
+  <h1><code>konnect-demo</code></h1>
+  <strong>An <code>npm init</code> template demonstrating json array flattening using Webassembly.</strong>
+</div>
+
+## About
+
+This template is designed for depending on NPM packages that contain
+Rust-generated WebAssembly and using them to create a Website.
+
+* Want to create an NPM package with Rust and WebAssembly? [Check out
+  `wasm-pack-template`.](https://github.com/rustwasm/wasm-pack-template)
+* Want to make a monorepo-style Website without publishing to NPM? Check out
+  [`rust-webpack-template`](https://github.com/rustwasm/rust-webpack-template)
+  and/or
+  [`rust-parcel-template`](https://github.com/rustwasm/rust-parcel-template).
+
+## 🚴 Usage
+
+
+```
+git clone <this repository>
+```
+```
+npm install && npm run 
+```
+
+## 🔋 Batteries Included
+
+
+## License
+
+Licensed under either of
+
+* MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally
+submitted for inclusion in the work by you, as defined in the Apache-2.0
+license, shall be dual licensed as above, without any additional terms or
+conditions.
---
+++ index.js
@@ -0,0 +1,88 @@
+import * as wasm from "@teekm/konnectwasm";
+
+/* Shopify example taken from https://shopify.dev/api/admin-rest */
+let example = {
+    "customers": [
+      {
+        "id": 207119551,
+        "email": "bob.norman@hostmail.com",
+        "accepts_marketing": false,
+        "created_at": "2021-02-12T13:48:32-05:00",
+        "updated_at": "2021-02-12T13:48:32-05:00",
+        "first_name": "Bob",
+        "last_name": "Norman",
+        "orders_count": 1,
+        "state": "disabled",
+        "total_spent": "199.65",
+        "last_order_id": 450789469,
+        "note": null,
+        "verified_email": true,
+        "multipass_identifier": null,
+        "tax_exempt": false,
+        "phone": "+16136120707",
+        "tags": "",
+        "last_order_name": "#1001",
+        "currency": "USD",
+        "addresses": [
+          {
+            "id": 207119551,
+            "customer_id": 207119551,
+            "first_name": null,
+            "last_name": null,
+            "company": null,
+            "address1": "Chestnut Street 92",
+            "address2": "",
+            "city": "Louisville",
+            "province": "Kentucky",
+            "country": "United States",
+            "zip": "40202",
+            "phone": "+1(800)-555-2181",
+            "name": "",
+            "province_code": "KY",
+            "country_code": "US",
+            "country_name": "United States",
+            "default": true
+          }
+        ],
+      },
+    ],
+    "accepts_marketing_updated_at": "2005-06-12T11:57:11-04:00",
+    "marketing_opt_in_level": null,
+    "tax_exemptions": [],
+    "admin_graphql_api_id": "gid://shopify/Customer/207119551",
+    "default_address": {
+      "id": 207119551,
+      "customer_id": 207119551,
+      "first_name": null,
+      "last_name": null,
+      "company": null,
+      "address1": "Chestnut Street 92",
+      "address2": "",
+      "city": "Louisville",
+      "province": "Kentucky",
+      "country": "United States",
+      "zip": "40202",
+      "phone": "+1(800)-555-2181",
+      "name": "",
+      "province_code": "KY",
+      "country_code": "US",
+      "country_name": "United States",
+      "default": true
+    }
+  }
+
+let data = wasm.process(JSON.stringify(example));
+let parsedData = JSON.parse(data);
+document.getElementById("output2").innerHTML = JSON.stringify(parsedData, undefined, 2);
+
+/* Larger file example - 2.5mb*/
+let url ='https://gist.githubusercontent.com/tuldok89/cf99b808ba0168054970bc60fbfb25c4/raw/ed635c262124fe88491ae25e56919b644994e66a/shopify.json';
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    let d = wasm.process(JSON.stringify(data));
+    let e = JSON.parse(d);
+    document.getElementById("output").innerHTML = JSON.stringify(e, undefined, 2);
+})
+.catch(e => console.dir(e))
